184809f31f9f95e62c87f28749dacefa85dd1445
Kasra Jannati <kasra_ja1991@yahoo.com>
2021-11-29 23:08:42 -0500

    update

--- components/home/header.js
+++ components/home/header.js
@@ -4,13 +4,17 @@ const Header = ({data}) => {
     return (
         <section className="header position-relative">
             <div className="video">
-                <iframe src="https://player.vimeo.com/video/524782434?muted=1&autoplay=1&controls=0&loop=1" 
+                {/* <iframe src="https://player.vimeo.com/video/524782434?muted=1&autoplay=1&controls=0&loop=1" 
                     frameBorder="0" 
                     webkitallowfullscreen="true"
                     mozallowfullscreen ="true"
                     allowFullScreen
                     title="cyber erth tech">
-                </iframe>
+                </iframe> */}
+                <video muted autoPlay playsInline={true} loop className="videoHomePage">
+                    <source src="./vid/fdtHomePage.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
             </div>
             <h1 className="text-uppercase">{data.h1}</h1>    
         </section>
