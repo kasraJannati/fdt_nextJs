3d281bc39d06df965906e7ed2f9e3fbe7856a0ba
Kasra <kasra_ja1991@yahoo.com>
2022-02-15 22:27:00 -0500

    update

--- components/home/ourclient.js
+++ components/home/ourclient.js
@@ -15,8 +15,8 @@ const Ourclient = ({data}) => {
                     <div className="col-lg-6">
                         <section className="clientslogo" >
                             {data.map((image,i) => (
-                                <a href={image.text} target="_blank">
-                                    <img src={image.media.filename} alt={image.alt} key={image.id} />
+                                <a href={image.text} target="_blank"  key={image.media.id}>
+                                    <img src={image.media.filename} alt={image.media.alt} />
                                 </a>
                             ))}
                         </section>
