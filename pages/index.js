a848ec19620166777e8b4813d7b3d9f5aca14e78
Kasra <kasra_ja1991@yahoo.com>
2022-06-17 21:00:31 -0400

    up

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
 import Head from 'next/head'
 import Layout from '../layouts/layout'
-// import Header from '../components/home/header'
-// import Description from '../components/home/description'
-// import Step from '../components/home/step'
-// import Ourclient from '../components/home/ourclient'
-// import Cursor from '../components/common/cursor'
-// import {storyblok} from '../config/storyblok';
+import Header from '../components/home/header'
+import Description from '../components/home/description'
+import Step from '../components/home/step'
+import Ourclient from '../components/home/ourclient'
+import Cursor from '../components/common/cursor'
+import {storyblok} from '../config/storyblok';
 
-// export async function getServerSideProps() {
-//   const res = await storyblok.get('cdn/stories/homepage/content', {})
-//   .then(response => {
-//     return response;
-//   }).catch(error => { 
-//     return error;
-//   })
-//   const data = res.data.story.content;
-//   return {
-//     props: {data}
-//   }
-// }
+export async function getServerSideProps() {
+  const res = await storyblok.get('cdn/stories/homepage/content', {})
+  .then(response => {
+    return response;
+  }).catch(error => { 
+    return error;
+  })
+  const data = res.data.story.content;
+  return {
+    props: {data}
+  }
+}
 
 export default function Home({data}) {
   let cursorTarget = ['.description','.ourclient'];
@@ -26,19 +26,19 @@ export default function Home({data}) {
   return (
     <div className="homepage">
       <Head>
-        {/* <title>FDT | {data.h1}</title>
-        <meta name="description" content={data.description} /> */}
+        <title>FDT | {data.h1}</title>
+        <meta name="description" content={data.description} />
       </Head>
-      {/* <Header data = {data}/>
-      <Description data = {data}/> */}
+      <Header data = {data}/>
+      <Description data = {data}/>
       <div className="stepLinkBox">
-          {/* <a href="/projects/smartcontracts" className="stepLink"><Step steps={data.smartcontracts}></Step></a>
+          <a href="/projects/smartcontracts" className="stepLink"><Step steps={data.smartcontracts}></Step></a>
           <a href="/projects/blockchain" className="stepLink"><Step steps={data.blockchain}></Step></a>
           <a href="/projects/production" className="stepLink"><Step steps={data.content_production}></Step></a>
-          <a href="/projects/comingsoon" className="stepLink"><Step steps={data.idea}></Step></a> */}
+          <a href="/projects/comingsoon" className="stepLink"><Step steps={data.idea}></Step></a>
       </div>
-      {/* <Ourclient data = {data.our_client}/> */}
-      {/* <Cursor cursorTarget={cursorTarget}/> */}
+      <Ourclient data = {data.our_client}/>
+      <Cursor cursorTarget={cursorTarget}/>
     </div>
   )
 }
